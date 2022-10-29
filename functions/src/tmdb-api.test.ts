d739ec763454b6af5b03b3ecbcab367767f9f64d
NewNoiseAnand <amallik@newnoiseworks.com>
2022-10-29 11:26:39 -0400

    rounds out tests on url methods

--- functions/src/tmdb-api.test.ts
+++ functions/src/tmdb-api.test.ts
@@ -1,12 +1,30 @@
 import {
   getMovieSearchUrl,
+  getPersonSearchUrl,
+  getMovieUrlById,
 } from "./tmdb-api"
 
 const tmdbApi = "https://test-themoviedb"
 const apiKey = "TMDB_API_KEY"
 
 describe("getMovieSearchUrl", () => {
-  test("Returns URL w/ API Key and query string attached correctly", () => {
+  test("Returns correct URL w/ proper params", () => {
     expect(getMovieSearchUrl("hey hey hey")).toEqual(`${tmdbApi}/3/search/movie?api_key=${apiKey}&query=hey%20hey%20hey`)
+  })
+})
+
+describe("getPersonSearchUrl", () => {
+  test("Returns correct URL w/ proper params", () => {
+    expect(getPersonSearchUrl("hey hey hey")).toEqual(`${tmdbApi}/3/search/person?api_key=${apiKey}&query=hey%20hey%20hey`)
+  })
+})
+
+describe("getMovieUrl", () => {
+  test("Returns correct URL w/ proper params", () => {
+    expect(getMovieUrlById(3)).toEqual(`${tmdbApi}/3/movie/3?api_key=${apiKey}&append_to_response=videos,images`)
+  })
+
+  test("Returns correct URL w/ credits w/ proper params", () => {
+    expect(getMovieUrlById(3, true)).toEqual(`${tmdbApi}/3/movie/3?api_key=${apiKey}&append_to_response=videos,credits,images`)
   })
 })
--- functions/src/tmdb-api.ts
+++ functions/src/tmdb-api.ts
@@ -9,12 +9,12 @@ function getPersonSearchUrl (query: string) {
   return `${tmdbApi}/3/search/person?api_key=${apiKey}&query=${encodeURIComponent(query)}`
 }
 
-function getMovieUrl (mid: string, withCredits?: boolean) {
-  return `${tmdbApi}/3/movie/${mid}?api_key=${apiKey}&append_to_response=videos${withCredits && ',credits'},images`
+function getMovieUrlById (mid: number, withCredits?: boolean) {
+  return `${tmdbApi}/3/movie/${mid}?api_key=${apiKey}&append_to_response=videos${withCredits ? ',credits' : ''},images`
 }
 
 export {
   getMovieSearchUrl,
   getPersonSearchUrl,
-  getMovieUrl,
+  getMovieUrlById,
 }
