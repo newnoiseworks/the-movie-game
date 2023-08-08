594dbcfe80d904b657758ec7b5cf743cf064d890
New Noise Anand <anand@newnoiseworks.com>
2023-08-08 16:29:00 -0400

    sets up app for testing against test db

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,9 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-require('dotenv').config({
+var dote = require('dotenv').config({
   path: './.env.test'
 })
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['./src']
 };
