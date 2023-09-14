7d66aeeeabdd53cc85a88c7b3fdc8d68ffad80cb
New Noise Anand <anand@newnoiseworks.com>
2023-09-14 01:12:26 -0400

    adds auth and proper-er game creation route

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,10 +2,25 @@ import axios from 'axios'
 
 import { auth } from './firebase'
 
-async function createGame() {
-  return axios
-    .get(`${process.env.REACT_APP_FUNCTIONS_URL}/createGame?uuid=${auth.currentUser?.uid!}&name=FirstUser`)
-    .then((response) => response.data)
+async function createGame(name: string) {
+  const headers = await getAuthHeaders()
+
+  const response = await axios
+    .post(
+      `${process.env.REACT_APP_FUNCTIONS_URL}/createGame`,
+      { name },
+      { headers }
+    )
+
+  return response.data
+}
+
+async function getAuthHeaders() {
+  const token = await auth.currentUser!.getIdToken()
+
+  return {
+    'Authorization': `Bearer ${token}`
+  }
 }
 
 export {
